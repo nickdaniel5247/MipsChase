fcb6b69aae9903f02acc5e424f3d65b0eb54d851
nickdaniel5247 <146045599+nickdaniel5247@users.noreply.github.com>
2025-03-30 17:00:39 -0700

    Rotate target to face hop direction

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -180,6 +180,12 @@ public class Target : MonoBehaviour
         case eState.kHopStart:
             //Determine where to hop
             DetermineHop();
+
+            //Face in hop direction
+            Vector2 direction = (m_vHopEndPos - m_vHopStartPos).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90f));
+
             m_nState = eState.kHop;
             break;
         case eState.kHop:
